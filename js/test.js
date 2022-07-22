70cb0b7ad1b8d06e285efed01376c9182a6c9ded
seminocoe <qhrms9595@naver.com>
2022-07-22 11:06:07 +0900

    Initial commit

---
+++ js/test.js
@@ -0,0 +1,10 @@
+/*스크립 주석은 이렇게 2개 가능*/
+        //기능을 넣을 수 있음
+      function show(){
+          //img 태그에 이미지를 보여주기
+          document.getElementById("fig").src="./img/Elvis.png"
+      }
+      function hide(){
+          //img 태그에 이미지를 숨기기
+          document.getElementById("fig").src=""
+      }
